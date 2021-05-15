c95d62ecfe7ff3f19aa0eb0d7fb7a66554037dbd
denis <denis@MacBook-Air-denis.local>
2021-05-15 15:26:30 +0200

    added userlist&userlistitem

---
+++ src/data/users.js
@@ -0,0 +1,32 @@
+export const users = [
+  {
+    name: 'Denys Horkavyi',
+    attendance: '39%',
+    average: '4,3',
+  },
+  {
+    name: 'Denys Horkavyi',
+    attendance: '39%',
+    average: '4,3',
+  },
+  {
+    name: 'Denys Horkavyi',
+    attendance: '39%',
+    average: '4,3',
+  },
+  {
+    name: 'Denys Horkavyi',
+    attendance: '39%',
+    average: '4,3',
+  },
+  {
+    name: 'Denys Horkavyi',
+    attendance: '39%',
+    average: '4,3',
+  },
+  {
+    name: 'Denys Horkavyi',
+    attendance: '39%',
+    average: '4,3',
+  },
+];
